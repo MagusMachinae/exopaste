3fb27ac76f079aa92d1bfcb53520075ba2f69dac
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-12-01 21:20:48 +0000

    main added

--- src/exopaste/main.clj
+++ src/exopaste/main.clj
@@ -1,7 +1,7 @@
-(ns exopaste.core
-  (:gen-class))
+(ns exopaste.main
+  (:require [exopaste.systen :refer [init-system start!]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (start!))
